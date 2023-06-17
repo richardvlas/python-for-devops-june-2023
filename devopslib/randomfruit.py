1a0c45898edda76ae93e1d73e2c01e73c83cb690
Richard Vlas <vlasrichard@gmail.com>
2023-06-17 14:39:09 +0000

    added command tool

--- devopslib/randomfruit.py
+++ devopslib/randomfruit.py
@@ -5,6 +5,13 @@ def fruit():
     fruits = ["apple", "banana", "cherry", "durian"]
     return choices(fruits)[0]
 
+def meal(beverage):
+    my_fruit = fruit()
+    print(f"My fruit is {my_fruit}")
+    if my_fruit == "cherry":
+        complete_meal = f"Your meal is a {my_fruit} and {beverage}"
+        return complete_meal
+    return f"Your meal is a steak and {beverage}"
 
 if __name__ == "__main__":
     print(fruit())
