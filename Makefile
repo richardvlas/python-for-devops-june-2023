11831919f22a9310715dfb1640e6da8f99c63cf9
Richard Vlas <vlasrichard@gmail.com>
2023-06-17 09:29:59 +0000

    adding lint

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 install:
 	pip install --upgrade pip &&\
 	pip install -r requirements.txt
+
+lint:
+	pylint --disable=R,C,W1203 *.py devopslib
 	
---
+++ devopslib/randomfruit.py
@@ -0,0 +1,9 @@
+from random import choices
+
+def fruit():
+    fruits = ['apple', 'banana', 'cherry', 'durian']
+    return choices(fruits)[0]
+
+
+if __name__ == '__main__':
+    print(fruit())
